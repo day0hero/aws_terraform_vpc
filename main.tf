7f0bf6d9b6c183f1599502b95964aa41470e28d9
jonny <jonny@redhat.com>
2021-06-28 20:42:38 -0500

    sanitized variable names

--- main.tf
+++ main.tf
@@ -3,12 +3,16 @@ provider "aws" {
 }
 
 module "ipi-vpc" {
-  source               = "./modules/ipi_vpc"
-  vpc_name             = "[ INSERT_VPC_NAME ]"
-  cluster_name         = "[ INSERT_OCP_OR_VPC_NAME_HERE]"
-  private_subnets      = local.private_subnets
-  public_subnets       = local.public_subnets
-  azs                  = local.azs
-  dns_support_enabled  = true
-  enable_dns_hostnames = true
+  source                      = "./modules/ipi_vpc"
+  vpc_name                    = "PROVIDE_VPC_NAME"
+  cluster_name                = "PROVIDE_VPC_OR_CLUSTER_NAME"
+  ssh_private_key_path        = "PATH_TO_SSH_PRIVATE_KEY"
+  bastion_public_ssh_key_name = "PROVIDE_SSH_PUBLIC_KEY_NAME"
+  bastion_instance_type       = "t3.large"
+  bastion_volume_size         = "80"
+  private_subnets             = local.private_subnets
+  public_subnets              = local.public_subnets
+  azs                         = local.azs
+  dns_support_enabled         = true
+  enable_dns_hostnames        = true
 }
