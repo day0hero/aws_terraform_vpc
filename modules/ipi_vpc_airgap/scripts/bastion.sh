9efb624f83f33545c960a516a5ee11b42986742e
jonny <jonny@redhat.com>
2021-07-01 23:21:27 -0500

    shortened script name and used absolut paths for directories

---
+++ modules/ipi_vpc_airgap/scripts/bastion.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Download the openshift installer and CLI"
+curl -LfO http://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest-4.7/openshift-install-linux.tar.gz
+curl -LfO http://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest-4.7/openshift-client-linux.tar.gz
+
+echo "Unpack the tarballs into /usr/local/bin/"
+sudo tar xvf openshift-install-linux.tar.gz -C /usr/local/bin/
+sudo tar xvf openshift-client-linux.tar.gz -C /usr/local/bin
+
+echo "Clean Up the download artifacts"
+rm -rf ~/openshift-*
+
+echo "Install some core packages and run yum update"
+sudo dnf install -y podman git vim tmux
+sudo dnf update -y --security
+
+echo "Create deployment scaffolding"
+mkdir /home/ec2-user/backup
+mkdir /home/ec2-user/deployment
+mkdir /home/ec2-user/.aws
